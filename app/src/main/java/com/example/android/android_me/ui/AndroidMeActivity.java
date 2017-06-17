c13c2cb805f1a7d4d2ee4b099336cfc9da7e02fc
Nikhil Bansal <nikhil97.nb@gmail.com>
2017-06-17 15:58:26 +0530

    All TODOs completed!

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.android_me.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -40,5 +41,12 @@ public class AndroidMeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_android_me);
 
         // TODO (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        BodyPartFragment bodyPartFragment = new BodyPartFragment();
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        manager.beginTransaction()
+                .add(R.id.fragment_body_container,bodyPartFragment)
+                .commit();
     }
 }
